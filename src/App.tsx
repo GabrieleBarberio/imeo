655b15171a146d1b7335d0cf8e3e1a598f00f3fb
grinker <gabriele.barberio@gmail.com>
2023-07-13 17:02:09 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className="bg-green-300 border-green-600 border-b p-4 m-4 rounded">
+  Hello World, wellcome to IMEO!
+</div>
+    </>
+  )
+}
+
+export default App
