1d000aaf0bfd955eef1c8df6d020cdcfd67697d7
DanieleLupo <danielelupo219@gmail.com>
2023-07-25 10:56:40 +0200

    feat: add carousel component

---
+++ src/component/Carousel.tsx
@@ -0,0 +1,11 @@
+function Carousel () {
+    return ( //ask Gabriele if we can install libraries
+        <Carousel>
+        <div>
+
+        </div>
+        </Carousel>
+    )
+}
+
+export default Carousel;
