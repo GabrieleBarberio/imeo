bb75bbb0f4e3976457275e33e53143031e7e0e11
DanieleLupo <danielelupo219@gmail.com>
2023-07-27 10:40:50 +0200

    feat: add aboutUs component

---
+++ src/component/AboutUs.tsx
@@ -0,0 +1,117 @@
+import member from "../assets/member.png"
+
+function AboutUs () {
+    return (
+        <>
+        <section className="mb-[160px] bg-[#FFFDFA]">
+            <div className="py-24">
+                <h1 className="text-lg font-bold">ABOUT US</h1>
+            </div>
+            <div className="flex gap-12">
+                <div className="card flex flex-col items-center relative mt-14">
+                    <div className="card__portrait w-[5vw] absolute z-10 bottom-60">
+                        <img className="w-full h-full rounded-full" src={member} alt="Member portrait" />
+                    </div>
+                    <div className="w-full h-11 absolute rounded-t-lg bg-[#8076A9]"></div>
+                    <div className="py-4 px-4 rounded-lg mt-10 bg-[#D9D9D9] inline-block">
+                        <h2 className="text-lg font-bold my-4 mr-8">Gabriele Barberio</h2>
+                        <p className="min-w-full w-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="card__links">
+                            <div className="card__links__github">
+                                <img />
+                                <a href="#">@GabrieleBarberio</a>
+                            </div>
+                            <div className="card__links__linkedin">
+                                <img />
+                                <a href="#">@GabrieleBarberio</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card flex flex-col items-center relative mt-14">
+                    <div className="card__portrait w-[5vw] absolute z-10 bottom-60">
+                        <img className="w-full h-full rounded-full" src={member} alt="Member portrait" />
+                    </div>
+                    <div className="w-full h-11 absolute rounded-t-lg bg-[#8076A9]"></div>
+                    <div className="py-4 px-4 rounded-lg mt-10 bg-[#D9D9D9] inline-block">
+                        <h2 className="text-lg font-bold my-4 mr-8">Gabriele Barberio</h2>
+                        <p className="min-w-full w-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="card__links">
+                            <div className="card__links__github">
+                                <img />
+                                <a href="#">@GabrieleBarberio</a>
+                            </div>
+                            <div className="card__links__linkedin">
+                                <img />
+                                <a href="#">@GabrieleBarberio</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card flex flex-col items-center relative mt-14">
+                    <div className="card__portrait w-[5vw] absolute z-10 bottom-60">
+                        <img className="w-full h-full rounded-full" src={member} alt="Member portrait" />
+                    </div>
+                    <div className="w-full h-11 absolute rounded-t-lg bg-[#8076A9]"></div>
+                    <div className="py-4 px-4 rounded-lg mt-10 bg-[#D9D9D9] inline-block">
+                        <h2 className="text-lg font-bold my-4 mr-8">Gabriele Barberio</h2>
+                        <p className="min-w-full w-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="card__links">
+                            <div className="card__links__github">
+                                <img />
+                                <a href="#">@GabrieleBarberio</a>
+                            </div>
+                            <div className="card__links__linkedin">
+                                <img />
+                                <a href="#">@GabrieleBarberio</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card flex flex-col items-center relative mt-14">
+                    <div className="card__portrait w-[5vw] absolute z-10 bottom-60">
+                        <img className="w-full h-full rounded-full" src={member} alt="Member portrait" />
+                    </div>
+                    <div className="w-full h-11 absolute rounded-t-lg bg-[#8076A9]"></div>
+                    <div className="py-4 px-4 rounded-lg mt-10 bg-[#D9D9D9] inline-block">
+                        <h2 className="text-lg font-bold my-4 mr-8">Gabriele Barberio</h2>
+                        <p className="min-w-full w-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="card__links">
+                            <div className="card__links__github">
+                                <img />
+                                <a href="#">@GabrieleBarberio</a>
+                            </div>
+                            <div className="card__links__linkedin">
+                                <img />
+                                <a href="#">@GabrieleBarberio</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card flex flex-col items-center relative mt-14">
+                    <div className="card__portrait w-[5vw] absolute z-10 bottom-60">
+                        <img className="w-full h-full rounded-full" src={member} alt="Member portrait" />
+                    </div>
+                    <div className="w-full h-11 absolute rounded-t-lg bg-[#8076A9]"></div>
+                    <div className="py-4 px-4 rounded-lg mt-10 bg-[#D9D9D9] inline-block">
+                        <h2 className="text-lg font-bold my-4 mr-8">Gabriele Barberio</h2>
+                        <p className="min-w-full w-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="card__links">
+                            <div className="card__links__github">
+                                <img />
+                                <a href="#">@GabrieleBarberio</a>
+                            </div>
+                            <div className="card__links__linkedin">
+                                <img />
+                                <a href="#">@GabrieleBarberio</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default AboutUs;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { CallToAction } from "../component/CallToAction";
+import AboutUs from "../component/AboutUs";
 
 export const Home = () => {
   return (
     <>
       <div className="w-[100vw] h-[100vh]  bg-[#000814]">
         <CallToAction />
+        <AboutUs />
       </div>
     </>
   );
