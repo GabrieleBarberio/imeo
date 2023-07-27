93b8a914924ffb77ccbece1183652a206161f85f
DanieleLupo <danielelupo219@gmail.com>
2023-07-27 13:03:14 +0200

    feat: add carousel component

---
+++ src/component/LandingCarousel.tsx
@@ -0,0 +1,24 @@
+import { Carousel } from 'flowbite-react';
+
+export default function LandingCarousel() {
+  return (
+    <Carousel>
+      <img
+        alt="..."
+        src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+      />
+      <img
+        alt="..."
+        src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+      />
+      <img
+        alt="..."
+        src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+      />
+      <img
+        alt="..."
+        src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+      />
+    </Carousel>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import { CallToAction } from "../component/CallToAction";
+import LandingCarousel from "../component/LandingCarousel";
 
 export const Home = () => {
   return (
     <>
       <div className="w-[100vw] h-[100vh]  bg-[#000814]">
-        <CallToAction />
+        <LandingCarousel />
       </div>
     </>
   );
