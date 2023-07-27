d103d4b4982f5b2da1423284fd1bbc517ea062af
DanieleLupo <danielelupo219@gmail.com>
2023-07-27 11:09:58 +0200

    feat: add footer component

---
+++ src/component/Footer.tsx
@@ -0,0 +1,30 @@
+function Footer () {
+    return (
+        <>
+        <section className="bg-[#F5ECFF]">
+            <div className="flex gap-80">
+                <div className="ml-20 py-32 px-24">
+                    <h2 className="text-3xl font-bold">IMEO</h2>
+                    <span className="text-3xl font-bold">Costruita per i team di <br/>sviluppo.</span>
+                </div>
+                <div className="flex gap-24 py-16 px-24">
+                    <ul>
+                        <li><h2 className="text-3xl font-bold">Customer</h2></li>
+                        <li className="mt-4"><a className="text-[#00000080] text-xl font-bold">Works</a></li>
+                        <li className="mt-4"><a className="text-[#00000080] text-xl font-bold">Strategy</a></li>
+                        <li className="mt-4"><a className="text-[#00000080] text-xl font-bold">Releases</a></li>
+                    </ul>
+                    <ul>
+                        <li><h2 className="text-3xl font-bold">Customer</h2></li>
+                        <li className="mt-4"><a className="text-[#00000080] text-xl font-bold">Works</a></li>
+                        <li className="mt-4"><a className="text-[#00000080] text-xl font-bold">Strategy</a></li>
+                        <li className="mt-4"><a className="text-[#00000080] text-xl font-bold">Releases</a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default Footer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import { CallToAction } from "../component/CallToAction";
+import Footer from "../component/Footer";
 
 export const Home = () => {
   return (
     <>
       <div className="w-[100vw] h-[100vh]  bg-[#000814]">
-        <CallToAction />
+        <Footer />
       </div>
     </>
   );
