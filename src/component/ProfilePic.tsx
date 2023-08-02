a5671af9538cd7316802566af10c142f3864344a
daveinthahood <davidesimone96@outlook.it>
2023-08-02 18:09:21 +0200

    add pic weight

--- src/component/ProfilePic.tsx
+++ src/component/ProfilePic.tsx
@@ -1,8 +1,16 @@
-
+import GabrieleBarberio from "../assets/GabrieleBaberio.png"
 
 export const ProfilePic = () => {
     return (
         <>
+            {/* PROFILE PIC MED */}
+                <img src={GabrieleBarberio} alt="picmed" className="w-[120px] rounded-xl" />
+
+            {/* PROFILE PIC LARGE */}
+                <img src={GabrieleBarberio} alt="piclarge" className="w-[150px] rounded-xl" />
+
+            {/* PROFILE PIC SMALL */}
+                <img src={GabrieleBarberio} alt="picsmall" className="w-[90px] rounded-xl" />
         </>
     )
 }
